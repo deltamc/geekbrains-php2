7449bbc3b9dc756cc708cf34c6294ab3f6e58464
danila k <danila k>
2017-08-20 19:07:30 +0300

    add

--- 2/1.php
+++ 2/1.php
@@ -1,11 +1,16 @@
 <?php
 abstract class Product
 {
-    protected $price;
+    private $price;
 
     public function setPrice($price)
     {
         $this->price = $price;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
     }
 
     abstract public function calculation();
@@ -20,16 +25,21 @@ abstract class Product
 
 class ProductWeight extends Product
 {
-    protected $weight;
+    private $weight;
 
     public function setWeight($weight)
     {
         $this->weight = $weight;
     }
 
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
     public function calculation()
     {
-        return $this->price * $this->weight;
+        return $this->getPrice() * $this->getWeight();
     }
 
 }
@@ -38,7 +48,7 @@ class ProductDigital extends Product
 {
     public function calculation()
     {
-        return $this->price/2;
+        return $this->getPrice()/2;
     }
 }
 
@@ -46,6 +56,6 @@ class ProductStandard extends Product
 {
     public function calculation()
     {
-        return $this->price;
+        return $this->getPrice();
     }
 }
